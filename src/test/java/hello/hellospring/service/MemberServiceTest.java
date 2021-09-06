2db0a3c0c0c08f8e6cc34ceb0332774db59eb65c
Black_Bong <shoy1415@naver.com>
2021-09-06 09:28:43 +0900

    스프링 기초 시작

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,37 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member members = memberService.findOne(saveId).get();
+
+        assertThat(member.getName()).isEqualTo(members.getName());
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
